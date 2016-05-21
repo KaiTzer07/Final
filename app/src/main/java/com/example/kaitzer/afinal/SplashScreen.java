7fa53035679dcfbc9c6f5884eaeea67b80e40179
KaiTzer <kaitzer@msn.com>
2016-05-21 10:35:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kaitzer/afinal/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.kaitzer.afinal;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    ImageView icon;
+    public static int SPLASH_TIME_OUT = 3500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        icon = (ImageView) findViewById(R.id.IVicon);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
