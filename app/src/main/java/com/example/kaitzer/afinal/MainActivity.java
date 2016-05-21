678382a372e8d0b4f809d54c27b349311850a6be
pewpewmeow <yushaun@ymail.com>
2016-05-21 11:07:02 +0800

    test

--- app/src/main/java/com/example/kaitzer/afinal/MainActivity.java
+++ app/src/main/java/com/example/kaitzer/afinal/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //iiiiii
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
