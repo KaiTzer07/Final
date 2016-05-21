c28737aafa358802b572f980d5f1f38b7e7fd571
KaiTzer <kaitzer@msn.com>
2016-05-21 11:19:35 +0800

    CHange 2

--- app/src/main/java/com/example/kaitzer/afinal/MainActivity.java
+++ app/src/main/java/com/example/kaitzer/afinal/MainActivity.java
@@ -4,17 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-///jfjfddh
-////jgfhdhfd
-
-    //test2
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    ///waqas
-        //change
     }
 }
