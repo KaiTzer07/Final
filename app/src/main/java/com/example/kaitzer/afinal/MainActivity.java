db0c6e5121ca9b615f1259f676eb5169cf3015fe
Waqas Khalid Obeidy <waquaskhalid@yahoo.com>
2016-05-21 11:16:51 +0800

    testttfgd

--- app/src/main/java/com/example/kaitzer/afinal/MainActivity.java
+++ app/src/main/java/com/example/kaitzer/afinal/MainActivity.java
@@ -8,9 +8,12 @@ public class MainActivity extends AppCompatActivity {
 ////jgfhdhfd
 
     //test2
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    ///waqas
     }
 }
