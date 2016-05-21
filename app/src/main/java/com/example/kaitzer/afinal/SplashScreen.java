f6ff3a8fdc3c81d0f2274cb30dec0fe7213e0b52
pewpewmeow <yushaun@ymail.com>
2016-05-21 16:09:34 +0800

    icons

--- app/src/main/java/com/example/kaitzer/afinal/SplashScreen.java
+++ app/src/main/java/com/example/kaitzer/afinal/SplashScreen.java
@@ -4,20 +4,28 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.OvershootInterpolator;
 import android.widget.ImageView;
+
+import com.github.florent37.viewanimator.ViewAnimator;
 
 public class SplashScreen extends AppCompatActivity {
 
     ImageView icon;
-    public static int SPLASH_TIME_OUT = 2000;
+    public static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppThemeNoBar);
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_splash_screen);
 
         icon = (ImageView) findViewById(R.id.IVicon);
 
+        animateParallel();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -31,5 +39,23 @@ public class SplashScreen extends AppCompatActivity {
     {
         super.onPause();
         finish();
+    }
+
+    protected void animateParallel() {
+        ViewAnimator.animate(icon)
+                .dp().translationY(-1000, 0)
+                .alpha(0, 1)
+                .singleInterpolator(new OvershootInterpolator())
+
+                .waitForHeight()
+                .singleInterpolator(new AccelerateDecelerateInterpolator())
+                .duration(2000)
+
+                .andAnimate(icon)
+                .rotation(0, 720)
+
+                .duration(2500)
+
+                .start();
     }
 }
